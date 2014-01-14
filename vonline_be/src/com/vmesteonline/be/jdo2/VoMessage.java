8f83764e8268f3108db20c9197337c25ae3e0965
trifidtrifid <trifidtrifid@gmail.com>
2014-01-14 23:58:26 +0400

    data

---
+++ vonline_be/src/com/vmesteonline/be/jdo2/VoMessage.java
@@ -0,0 +1,80 @@
+package com.vmesteonline.be.jdo2;
+
+import com.google.appengine.api.datastore.Key;
+import com.google.appengine.api.datastore.KeyFactory;
+import com.vmesteonline.be.data.PMF;
+
+import javax.jdo.PersistenceManager;
+import javax.jdo.PersistenceManagerFactory;
+import javax.jdo.annotations.IdGeneratorStrategy;
+import javax.jdo.annotations.PersistenceCapable;
+import javax.jdo.annotations.Persistent;
+import javax.jdo.annotations.PrimaryKey;
+
+import org.apache.log4j.Logger;
+
+
+/**
+ * Created by brozer on 1/12/14.
+ */
+@PersistenceCapable
+public class VoMessage extends com.vmesteonline.be.Message {
+
+	private static Logger logger = Logger.getLogger("com.vmesteonline.be.jdo2.VoMessage");
+	
+	public VoMessage(com.vmesteonline.be.Message msg ){
+		if( 0!=msg.getParentId() ){
+			PersistenceManagerFactory pmf = PMF.get();
+			PersistenceManager pm = pmf.getPersistenceManager();
+			try {
+				//Key parentKey = KeyFactory.createKey(VoMessage.class.getSimpleName(), msg.getParentId());
+				VoMessage parentMsg = pm.getObjectById(VoMessage.class,	msg.getParentId());
+			} catch (Exception e) {
+				//throw new 
+			}
+			
+			
+		}
+	}
+	
+    @PrimaryKey
+    @Persistent(valueStrategy = IdGeneratorStrategy.INCREMENT)
+    private Key key;
+
+    @Persistent
+    private String streetAddress;
+
+    @Persistent
+    private Key parentId; // 'идентификатор родительского сообщения, NULL для корневого со',
+    @Persistent
+    private Key topicId;
+
+    @Persistent
+    private VoUser author; //'автор сообщения или темы',
+
+    @Persistent
+    private VoUser recipientId; // 'адресат задан только для личных сообщений, иначе NULL',
+    @Persistent
+    private int created; // 'дата создания',
+    @Persistent
+    private int edited;
+    @Persistent
+    private VoUser approved; // 'идентификатор пользователя промодерировавшего сообщение',
+    @Persistent
+    private byte[] content; // 'содержание сообщения',
+    @Persistent
+    private int likes;
+    @Persistent
+    private int unlikes;
+    @Persistent
+    private Key idForum;
+    @Persistent
+    private Key idShop;
+    @Persistent
+    private Key idDialog;
+    @Persistent
+    private Key idNews;
+
+    @Persistent
+    private RubricLocation location;
+}
