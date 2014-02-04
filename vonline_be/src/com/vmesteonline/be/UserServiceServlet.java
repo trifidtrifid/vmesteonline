f4412b2494bccbf8fcca8d3df3e0e79b865660d3
trifidtrifid <trifidtrifid@gmail.com>
2014-02-04 11:47:57 +0400

    rename user service

--- vonline_be/src/com/vmesteonline/be/UserServiceServlet.java
+++ vonline_be/src/com/vmesteonline/be/UserServiceServlet.java
@@ -2,13 +2,13 @@ package com.vmesteonline.be;
 
 import org.apache.thrift.protocol.TJSONProtocol;
 
-public class UserSericeServlet extends VoServlet {
+public class UserServiceServlet extends VoServlet {
 
-	public UserSericeServlet() {
+	public UserServiceServlet() {
 		super(new TJSONProtocol.Factory());
-		AuthServiceImpl servImpl = new AuthServiceImpl();
+		UserServiceImpl servImpl = new UserServiceImpl();
 		serviceImpl = servImpl;
-		super.setProcessor(new AuthService.Processor<AuthServiceImpl>(servImpl));
+		super.setProcessor(new UserService.Processor<UserServiceImpl>(servImpl));
 	}
 
 	private static final long serialVersionUID = 988473042573260646L;
