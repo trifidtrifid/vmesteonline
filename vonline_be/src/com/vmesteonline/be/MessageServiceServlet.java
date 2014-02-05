3d4ba85331093e999fcfd3c4d8918da7f4b56c0b
Anton <antoha_r@mail.ru>
2014-02-05 16:24:25 +0400

    Изменения в MessageServiceServlet

--- vonline_be/src/com/vmesteonline/be/MessageServiceServlet.java
+++ vonline_be/src/com/vmesteonline/be/MessageServiceServlet.java
@@ -3,9 +3,16 @@ package com.vmesteonline.be;
 import org.apache.thrift.protocol.TJSONProtocol;
 import org.apache.thrift.protocol.TProtocolFactory;
 
-public class MessageServiceServlet extends VoServlet {	
-	
-	public MessageServiceServlet(TProtocolFactory protocolFactory) {
+public class MessageServiceServlet extends VoServlet {
+
+    public MessageServiceServlet() {
+        super(new TJSONProtocol.Factory());
+        MessageServiceImpl servImpl = new MessageServiceImpl();
+        serviceImpl = servImpl;
+        super.setProcessor(new MessageService.Processor<MessageServiceImpl>(servImpl));
+    }
+
+    public MessageServiceServlet(TProtocolFactory protocolFactory) {
 		super(new TJSONProtocol.Factory());		
 		MessageServiceImpl servImpl = new MessageServiceImpl();
 		serviceImpl = servImpl;
