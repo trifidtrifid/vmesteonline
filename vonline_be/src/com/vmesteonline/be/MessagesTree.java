ecee1fb717ad2c226f7a1f8619dedcdd1217e77f
trifid <trifid@trifid-ws.(none)>
2014-02-12 17:52:02 +0400

    add tree msg representation

--- vonline_be/src/com/vmesteonline/be/MessagesTree.java
+++ vonline_be/src/com/vmesteonline/be/MessagesTree.java
@@ -1,22 +1,67 @@
 package com.vmesteonline.be;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
+import com.google.appengine.api.datastore.Key;
 import com.vmesteonline.be.jdo2.VoMessage;
 
 public class MessagesTree {
 
+	public MessagesTree(List<VoMessage> vomsgs) {
+		msgs = vomsgs;
+		items = new ArrayList<ItemPosition>();
+		List<VoMessage> firstLevel = getLevel(0);
+		parseLevel(firstLevel, 0);
+	}
+
+//	public get
+	
+	private void parseLevel(List<VoMessage> levelMsgs, int level) {
+		Collections.sort(levelMsgs, new ByCreateTimeComparator());
+		for (VoMessage voMsg : levelMsgs) {
+			items.add(new ItemPosition(voMsg.getId().getId(), 0, level));
+			List<VoMessage> nextLevel = getLevel(voMsg.getId().getId());
+			parseLevel(nextLevel, level + 1);
+		}
+	}
+
+	private List<VoMessage> getLevel(long parentId) {
+		List<VoMessage> l = new ArrayList<VoMessage>();
+		for (VoMessage m : msgs) {
+			if (m.getParentId() == parentId)
+				l.add(m);
+		}
+		return l;
+	}
+
+	class ByCreateTimeComparator implements Comparator<VoMessage> {
+
+		@Override
+		public int compare(VoMessage a, VoMessage b) {
+			return a.getCreatedAt() < b.getCreatedAt() ? 0 : 1;
+		}
+
+	}
+
+	class ItemPosition {
+		public ItemPosition(long id, long parentId, int level) {
+			this.id = id;
+			this.parentId = parentId;
+			this.level = level;
+		}
+
+		public long id;
+		public long parentId;
+		public int level;
+
+	}
+
 	protected MessagesTree() {
 	}
 
-	public MessagesTree(List<VoMessage> vomsgs) {
-
-	}
-
-	class ItemPosition{
-		
-	}
-	
-	
 	protected List<ItemPosition> items;
+	protected List<VoMessage> msgs;
 }
--- vonline_be/tests/com/vmesteonline/be/MessagesTreeTests.java
+++ vonline_be/tests/com/vmesteonline/be/MessagesTreeTests.java
@@ -3,21 +3,27 @@ package com.vmesteonline.be;
 import java.util.ArrayList;
 import java.util.List;
 
+import junit.framework.Assert;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 import com.google.appengine.api.datastore.KeyFactory;
+import com.google.appengine.tools.development.testing.LocalDatastoreServiceTestConfig;
+import com.google.appengine.tools.development.testing.LocalServiceTestHelper;
 import com.vmesteonline.be.jdo2.VoMessage;
 
 public class MessagesTreeTests extends MessagesTree {
 
 	List<VoMessage> lst = new ArrayList<VoMessage>();
-
+	int msgCreateTime;
 	VoMessage createVoMsg(long id, long parentId) {
 		VoMessage msg = new VoMessage();
 		msg.setId(KeyFactory.createKey(VoMessage.class.toString(), id));
 		msg.setParentId(parentId);
+		msgCreateTime += 10;
+		msg.setCreatedAt(msgCreateTime);
 		return msg;
 	}
 
@@ -33,6 +39,8 @@ public class MessagesTreeTests extends MessagesTree {
 
 	@Before
 	public void setUp() throws Exception {
+		helper.setUp();
+		msgCreateTime = 0;
 		lst.add(createVoMsg(1, 0));
 		lst.add(createVoMsg(2, 0));
 		lst.add(createVoMsg(3, 0));
@@ -45,11 +53,21 @@ public class MessagesTreeTests extends MessagesTree {
 
 	@After
 	public void tearDown() throws Exception {
+		helper.tearDown();
 	}
 
 	@Test
 	public void testMessageTreeCreateObject() {
-		MessagesTree t = new MessagesTree(lst); 
+		MessagesTree t = new MessagesTree(lst);
+		Assert.assertEquals(8, t.items.size());
+		Assert.assertEquals(1, t.items.get(0).id);
+		Assert.assertEquals(4, t.items.get(1).id);
+		Assert.assertEquals(5, t.items.get(2).id);
+		Assert.assertEquals(6, t.items.get(3).id);
+
 	}
+
+	private final LocalServiceTestHelper helper = new LocalServiceTestHelper(
+			new LocalDatastoreServiceTestConfig());
 
 }
