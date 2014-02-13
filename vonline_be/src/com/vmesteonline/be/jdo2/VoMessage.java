be8a4bd332a15b82adf0f1a37bc434461b73fae8
trifidtrifid <trifidtrifid@gmail.com>
2014-02-13 16:55:40 +0400

    return parent id in message correctly

--- vonline_be/src/com/vmesteonline/be/jdo2/VoMessage.java
+++ vonline_be/src/com/vmesteonline/be/jdo2/VoMessage.java
@@ -124,8 +124,7 @@ public class VoMessage extends VoBaseMessage {
 	}
 
 	public Message getMessage() {
-		Key parentKey = id.getParent();
-		return new Message(id.getId(), null == parentKey ? 0L : parentKey.getId(), type, topicId, 0L, authorId.getId(), createdAt, editedAt, new String(
+		return new Message(id.getId(), getParentId(), type, topicId, 0L, authorId.getId(), createdAt, editedAt, new String(
 				content), getLikes(), getUnlikes(), links, tags, null, 0);
 	}
 
