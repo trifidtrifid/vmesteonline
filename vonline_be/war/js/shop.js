9343b3a84661296f69435b0a684fac7190ef0305
Anton <antoha_r@mail.ru>
2014-03-23 22:45:06 +0400

    фильтры при вводе данных для доставки

--- vonline_be/war/js/shop.js
+++ vonline_be/war/js/shop.js
@@ -99,20 +99,25 @@ $(document).ready(function(){
     var countries = addressesBase.countries;
     var countriesLength = countries.length;
     var countryTags = [],
-        countryId = [];
+        countryId = [],
+        cityTags = [],
+        cityId = [],
+        streetTags = [],
+        streetId = [];
 
     for (var i = 0; i < countriesLength; i++){
         //alert(countries[i].name);
         countryTags[i] = countries[i].name;
         countryId[i] = countries[i].id;
     }
+
     // временная тема, так как три России в пробном варианте
     /*countryTags.shift();
     countryTags.shift();
     countryId.shift();
     countryId.shift();
     countriesLength = countryTags.length;*/
-        var counter = 0,tempFlag = 1;
+      /*  var counter = 0,tempFlag = 1;
         var newCountryTags = [];
         for(i = 0; i < countriesLength-1; i++){
             console.log(countryTags[i]);
@@ -132,9 +137,11 @@ $(document).ready(function(){
             }
         }
         newCountryTags.push(countryTags[countriesLength-1]);
-        countryTags = newCountryTags;
-        //alert(countryTags.length);
+        countryTags = newCountryTags;*/
 
+    $( "#country-delivery" ).autocomplete({
+        source: countryTags
+    });
     $('#city-delivery').focus(function(){
         var prevField = $('#country-delivery').val();
         var cities;
@@ -152,15 +159,68 @@ $(document).ready(function(){
         var citiesLength = cities.length;
         var cityTags = [];
         for (i = 0; i < citiesLength; i++){
-            //alert(cities[i].name+" "+cities[i].countryId);
             cityTags[i] = cities[i].name;
         }
 
-        $( "#city-delivery" ).autocomplete({
+        $(this).autocomplete({
             source: cityTags
         });
     });
-    var streets = addressesBase.streets;
+
+    $( "#street-delivery" ).focus(function(){
+        var prevField = $('#city-delivery').val();
+        var streets;
+        var citiesLength = addressesBase.cities.length;
+        if(prevField){
+            for (var i = 0; i < citiesLength; i++){
+                if (prevField == cityTags[i]){
+                    streets = userServiceClient.getStreets(cityId[i]);
+                    break;
+                }
+            }
+        }
+        if (!streets){
+            streets = addressesBase.streets;
+        }
+        var streetsLength = streets.length;
+        var streetTags = [];
+        for (i = 0; i < streetsLength; i++){
+            streetTags[i] = streets[i].name;
+        }
+
+        $(this).autocomplete({
+            source: streetTags
+        });
+    });
+
+    $( "#building-delivery" ).focus(function(){
+            var prevField = $('#street-delivery').val();
+            var buildings;
+        var streetLength = addressesBase.streets.length;
+        if(prevField){
+                for (var i = 0; i < streetLength; i++){
+                    if (prevField == streetTags[i]){
+                        buildings = userServiceClient.getBuildings(streetId[i]);
+                        break;
+                    }
+                }
+            }
+            if (!buildings){
+                buildings = addressesBase.buildings;
+            }
+            var buildingsLength = buildings.length;
+            var buildingsTags = [];
+            for (i = 0; i < buildingsLength; i++){
+                buildingsTags[i] = buildings[i].fullNo;
+            }
+
+            $(this).autocomplete({
+                source: buildingsTags
+            });
+        });
+
+
+    /*var streets = addressesBase.streets;
     var streetsLength = streets.length;
     var streetTags = [];
     for (i = 0; i < streetsLength; i++){
@@ -173,15 +233,13 @@ $(document).ready(function(){
     for (i = 0; i < buildingsLength; i++){
         buildingTags[i] = buildings[i].fullNo;
     }
-    $( "#country-delivery" ).autocomplete({
-        source: countryTags
-    });
+
     $( "#street-delivery" ).autocomplete({
         source: streetTags
     });
     $( "#building-delivery" ).autocomplete({
         source: buildingTags
-    });
+    });*/
     }catch(e){
             alert(e+ " Ошибка autocomplete")
     }
