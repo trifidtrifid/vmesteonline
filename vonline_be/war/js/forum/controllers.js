caf864bdd93f42e7be1d05ed7558ba07c6f871b2
Anton <antoha_r@mail.ru>
2014-07-24 01:32:12 +0400

    meta

--- vonline_be/war/js/forum/controllers.js
+++ vonline_be/war/js/forum/controllers.js
@@ -1611,17 +1611,27 @@ function clone(obj){
         temp[key] = clone(obj[key]);
     return temp;
 }
-/*
-$.widget("custom.catcomplete", $.ui.autocomplete, {
-    _renderMenu: function(ul, items){
-        var self = this,
-            currentCategory = "";
-        $.each( items, function(index, item){
-            if(item.category != currentCategory){
-                ul.append( "<li class='ui-autocomplete-category'>" + item.category + "</li>" );
-                currentCategory = item.category;
-            }
-            self._renderItem( ul, item );
-        });
+function base64encode(str) {
+    // Символы для base64-преобразования
+    var b64chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefg'+
+        'hijklmnopqrstuvwxyz0123456789+/=';
+    var b64encoded = '';
+    var chr1, chr2, chr3;
+    var enc1, enc2, enc3, enc4;
+
+    for (var i=0; i<str.length;) {
+        chr1 = str.charCodeAt(i++);
+        chr2 = str.charCodeAt(i++);
+        chr3 = str.charCodeAt(i++);
+
+        enc1 = chr1 >> 2;
+        enc2 = ((chr1 & 3) << 4) | (chr2 >> 4);
+
+        enc3 = isNaN(chr2) ? 64:(((chr2 & 15) << 2) | (chr3 >> 6));
+        enc4 = isNaN(chr3) ? 64:(chr3 & 63);
+
+        b64encoded += b64chars.charAt(enc1) + b64chars.charAt(enc2) +
+            b64chars.charAt(enc3) + b64chars.charAt(enc4);
     }
-});*/
+    return b64encoded;
+}
