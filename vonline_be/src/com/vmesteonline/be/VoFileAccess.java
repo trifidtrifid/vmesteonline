2510416486366ab76381a31f86ad1d92c43e3ee1
Alexey Cherviakov <ceo@vmesteonline.ru>
2014-07-29 10:53:27 +0400

    file cache fix

--- vonline_be/src/com/vmesteonline/be/VoFileAccess.java
+++ vonline_be/src/com/vmesteonline/be/VoFileAccess.java
@@ -6,6 +6,7 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.PrintWriter;
+import java.io.Serializable;
 import java.util.List;
 
 import javax.jdo.PersistenceManager;
