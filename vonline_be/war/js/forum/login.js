51fe25e524e698c408f17527c29ebf6435d43d2f
Anton <antoha_r@mail.ru>
2014-07-30 20:29:56 +0400

    for issue #56

--- vonline_be/war/js/forum/login.js
+++ vonline_be/war/js/forum/login.js
@@ -1,11 +1,36 @@
 $(document).ready(function(){
+
     var transport = new Thrift.Transport("/thrift/AuthService");
     var protocol = new Thrift.Protocol(transport);
     var authClient = new com.vmesteonline.be.AuthServiceClient(protocol);
 
+    var URL = document.location.hash;
+
+    if(URL) {
+        var URLArray = URL.split(';');
+        var email = URLArray[0].split('=')[1],
+            mapUrl = URLArray[1].split('=')[1],
+            address = URLArray[2].split('=')[1],
+            locationId = URLArray[3].split('=')[1];
+
+        $('#email').val(email);
+        $('.mapUrl').attr('src', mapUrl);
+        $('.address').text(address);
+    }
+
     $('#login-box .btn-login').click(function(e){
         e.preventDefault();
         login($(this));
+    });
+
+    $('.btn-reg').click(function(){
+
+        try{
+            authClient.registerNewUser($('#ufirstname'), $('#ulastname'), $('#password'), $('#email'), locationId);
+        }catch(e){
+           $('.error-info').text('Ошибка регистрации. Вы указали не все данные').show();
+        }
+
     });
 
     function login(selector) {
