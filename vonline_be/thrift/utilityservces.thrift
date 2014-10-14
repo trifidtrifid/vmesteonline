5fdd132caf34b89e2bfa62e5dbc7a5994b0ae30f
Alexey Cherviakov <ceo@vmesteonline.ru>
2014-10-15 01:00:11 +0400

    tmp

--- vonline_be/thrift/utilityservces.thrift
+++ vonline_be/thrift/utilityservces.thrift
@@ -8,6 +8,7 @@ struct Counter {
 	2:string location, //кухня, туалет, ванная комната, холл
 	3:CounterType type, //тип счетчика
 	4:string number, //номер счетчика
+	5:double lastValue
 }
 
 service UtilityService {
@@ -15,7 +16,6 @@ service UtilityService {
 	i64 registerCounter( 1:Counter newCounter )throws (1:error.InvalidOperation exc), //создание счетчика в квартире текущего пользователя
 	
 	list<Counter> getCounters() throws (1:error.InvalidOperation exc), //возвращает счетчики на адресе текущего пользователя
-	map< Counter, double> getCounterValues() throws (1:error.InvalidOperation exc), //возвращает счетчики зарегистрированные на адресе у текущего пользователя
-	map< i32, double> getCounterHistory(1:i32 counterId, 2:i32 fromDate) throws (1:error.InvalidOperation exc), //возвращает историю показаний счетчика
+	map< i32, double> getCounterHistory(1:i32 counterId, 2:i32 fromDate, 3:i32 toDate) throws (1:error.InvalidOperation exc), //возвращает историю показаний счетчика
 	double setCurrentCounterValue(1:i64 counterId, 2:double counterValue, 3:i32 date) throws (1:error.InvalidOperation exc), //сохраняет показания счетчика и возвращает значение расхода. если данных за предыдущий период нет - возвращает 0
 }
