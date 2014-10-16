ce9d0613d117ba2d16ea01a70c237f213850da99
Anton <antoha_r@mail.ru>
2014-10-16 23:01:34 +0400

    register by house code some fix

--- vonline_be/war/js/forum/controllers.js
+++ vonline_be/war/js/forum/controllers.js
@@ -3032,8 +3032,11 @@ angular.module('forum.controllers', ['ui.select2','infinite-scroll','ngSanitize'
         };
         $rootScope.selectGroup(getBuildingGroup($rootScope.currentGroup));
     })
-    .controller('SetInfoController',function() {
+    .controller('SetInfoController',function($state, $rootScope) {
         var setInfo = this;
+
+        setInfo.isSaveResult = false;
+        setInfo.isError = false;
 
         setInfo.save = function(){
             var staircase, floor,flat;
@@ -3042,7 +3045,16 @@ angular.module('forum.controllers', ['ui.select2','infinite-scroll','ngSanitize'
             (!setInfo.floor) ? floor = 0 : floor = setInfo.floor;
             (!setInfo.flat) ? flat = 0 : flat = setInfo.flat;
 
-            userClient.updateUserAddress(staircase,floor,flat);
+            setInfo.isSaveResult = true;
+            //try {
+                userClient.updateUserAddress(staircase, floor, flat);
+                setInfo.info = "Сохранено";
+                window.location.replace('/');
+                //$rootScope.$apply();
+            /*}catch(e){
+                setInfo.info = "Произошла ошибка";
+                setInfo.isError = true;
+            }*/
         };
 
     })
